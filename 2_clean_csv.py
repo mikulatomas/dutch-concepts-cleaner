6968f1cd7c3c262483b0e1cb28c3ea98e68ea0c0
Tomáš Mikula <mail@tomasmikula.cz>
2020-11-05 22:26:37 +0100

    Update 2_clean_csv.py

--- 2_clean_csv.py
+++ 2_clean_csv.py
@@ -10,7 +10,7 @@ for subdir, dirs, files in os.walk('dataset'):
         if '.CSV' in filepath:
             target_dir = subdir.replace('dataset/cvsdata/', '')
             target_dir = os.path.join('clean_csv', target_dir)
-            print(f"Cleaning: {zippath}")
+            print(f"Cleaning: {filepath}")
             if not os.path.exists(target_dir):
                 os.makedirs(target_dir)
 
